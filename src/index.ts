7633852cf0d0cbf13922f1007bbe81a3fee09e69
natemoo-re <ntmoore@knox.edu>
2018-09-11 22:26:52 -0500

    feat(): add links

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,43 @@
 import { prompt } from './vendor/prompts';
-import { dim } from 'turbocolor';
+import { dim, bold } from 'turbocolor';
 import { readAbout } from './utils';
-
-
+import open from 'opn';
 
 
 async function run() {
     const about = await readAbout();
-    console.log();
-    console.log(`You have reached the digital resume of ${about.name}!`);
-    // console.log(about);
-    // const menu = {
-    //     type: 'select',
-    //     name: 'selected',
-    //     message: 'What would you like to see?',
-    //     choices: [
-    //         { title: `Social ${dim('Links')}`, value: 'social' },
-    //         { title: `Projects ${dim('Sites')}`, value: 'projects' },
-    //         { title: `Open Source ${dim('Github, NPM')}`, value: 'oss' },
-    //     ]
-    // }
 
-    // const { selected } = await prompt(menu);
-    // console.log(selected);
+    console.log();
+    console.log(bold(`${about.name}'s digital card\n`));
+
+    const menu = {
+        type: 'select',
+        name: 'selected',
+        message: `Explore`,
+        choices: [
+            { title: `links`, value: 'links' },
+            { title: `projects`, value: 'projects' }
+        ]
+    }
+
+    const { selected } = await prompt(menu);
+
+    switch (selected) {
+        case 'links':
+            const links = {
+                type: 'select',
+                name: 'url',
+                message: 'Open URL',
+                choices: Object.entries(about.links).map(([platform, data]) => ({ title: `${platform} ${dim(data.text)}`, value: data.url }))
+            }
+            const { url } = await prompt(links);
+            return open(url);
+        case 'projects':
+            console.log('Coming soon...');
+            break;
+    }
+    
+    return;
 };
 
 run();
