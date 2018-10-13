aa4f89cb424ea36b5472887128b7508d34f83b3e
natemoo-re <ntmoore@knox.edu>
2018-10-12 22:56:07 -0500

    rollup me.json

--- src/utils.ts
+++ src/utils.ts
@@ -2,14 +2,13 @@ import { resolve } from 'path';
 import { cwd } from 'process';
 import { promisify } from 'util';
 import { readFile as _readFile } from 'fs';
+import about from './me.json';
 const readFile = promisify(_readFile);
 
 import { About } from './models';
 
 export function readAbout(): Promise<About> {
-    console.log(cwd());
-    return readFile(resolve(cwd(), 'me.json'))
-        .then(buffer => JSON.parse(buffer.toString()));
+    return Promise.resolve(about);
 }
 
 export function isWin() {
