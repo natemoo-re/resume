64ec432cf537dcb00fbd15e91425ba862db23828
natemoo-re <ntmoore@knox.edu>
2018-10-12 22:41:47 -0500

    move me.json

--- src/utils.ts
+++ src/utils.ts
@@ -6,7 +6,7 @@ const readFile = promisify(_readFile);
 import { About } from './models';
 
 export function readAbout(): Promise<About> {
-    return readFile(join('dist', 'me.json'))
+    return readFile('me.json')
         .then(buffer => JSON.parse(buffer.toString()));
 }
 
